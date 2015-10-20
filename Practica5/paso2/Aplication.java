0d487d43b1139bef2d6d8eb861b55f9ebb70a851
Roberto Torres Jimenez <iruga_cross@hotmail.com>
2015-10-20 10:04:42 -0500

    Creación de la practica 5

---
+++ Practica5/paso2/Aplication.java
@@ -0,0 +1,28 @@
+/**
+* Autor: Roberto Torres Jimenez
+*
+* Clase libro del primer ejercicio de la segunda practica de la materia Analisis
+* y Diseño de Sistemas de Computación......
+*/
+
+public class Aplication{
+	Bookstore bs = new Bookstore();	
+	public static void main(String[] args){
+		Aplication app = new Aplication();
+		//inicia la aplicación
+		app.init();
+	}
+/**
+* Constructor de la clase
+*/
+	public Aplication(){
+	//nada que incializar
+	}
+/**
+* Este metodo inicia la aplicación
+*/
+	public void init(){
+		Menu menu = new Menu();
+		menu.despliegaMenu();
+	}
+}
---
+++ Practica5/paso2/Ventana.java
@@ -0,0 +1,4 @@
+/**
+* Author: Roberto Torres Jiménez
+* Clase ventana de la practica 5 de la materia AyDSC
+*/
